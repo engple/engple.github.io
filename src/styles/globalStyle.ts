91d21f15e9054ff5f400c93baec63cecb4d2b5fe
sjquant <seonujang92@gmail.com>
2024-08-10 15:26:06 +0900

    🔍 Add '영어표현' heading

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -9,7 +9,7 @@ const GlobalStyle = createGlobalStyle`
 
     --width: 980px;
     --min-width: 320px;
-    --post-width: 650px;
+    --post-width: 680px;
     --nav-height: 54px;
     --footer-height: 60px;
 
